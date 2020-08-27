516ff6419d28c5f86d571dbcdcbc9d24204090e0
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-08-27 20:04:31 +0530

    Update Email.java

--- Email.java
+++ Email.java
@@ -1,1 +1,13 @@
 // Code goes here
+public class Email {
+  private String firstName;
+  private String ;
+  private String ;
+  private String ;
+  private String ;
+  private String ;
+  
+  
+
+
+}
