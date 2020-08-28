ed4c8939dbc08f806e1838962009f73e9dbcca7e
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-08-28 13:33:17 +0530

    Update Email_Main.java

--- Email_Main.java
+++ Email_Main.java
@@ -4,7 +4,7 @@ public class Email App
 public static void main(String[]args)
 {
   Email em = new em("kevin","peter");
-  System.out.println(em.shoeinfo());
+  System.out.println(em.showinfo());
   
   
   
